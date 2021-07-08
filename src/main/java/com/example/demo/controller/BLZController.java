adb71cf6c799ef94f355d66600d99bdb04f07dda
feng.i.liu <feng.i.liu@accenture.com>
2021-07-08 10:09:33 +0800

    BLZ 学习请求魔兽世界接口

---
+++ src/main/java/com/example/demo/controller/BLZController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.spring.web.json.Json;
+
+import javax.swing.text.html.HTML;
+
+/**
+ * @ClassName: BLZController
+ * @Author: feng.i.liu
+ * @Description:
+ * @Date: 2021/6/30 16:49
+ * @Version: 1.0
+ */
+@RestController
+public class BLZController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    String url = "https://cn.battle.net";
+
+
+    @RequestMapping(value = "/blz",method = RequestMethod.GET)
+    public String test(){
+        return restTemplate.getForObject(url + "/wow/boss", String.class);
+    }
+}
