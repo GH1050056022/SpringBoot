d455536ef2f612e1a4994081c9351a939f1dba31
feng.i.liu <feng.i.liu@accenture.com>
2021-07-08 10:08:45 +0800

    swagger引入

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName: SwaggerConfig
+ * @Author: feng.i.liu
+ * @Description:
+ * @Date: 2021/6/30 15:04
+ * @Version: 1.0
+ */
+@Configuration //配置类
+public class SwaggerConfig {
+}
