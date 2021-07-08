1763d86071813884f0ccfe3b0e2a01201d2d9f0c
feng.i.liu <feng.i.liu@accenture.com>
2021-07-08 10:08:23 +0800

    异步请求

---
+++ src/main/java/com/example/demo/controller/AsyncController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.AsyncService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: AsyncController
+ * @Author: feng.i.liu
+ * @Description:
+ * @Date: 2021/7/1 17:20
+ * @Version: 1.0
+ */
+@RestController
+public class AsyncController {
+
+    @Autowired
+    AsyncService asyncService;
+
+    @GetMapping("/async/hello")
+    public String hello(){
+        asyncService.hello();
+        return "success";
+    }
+}
---
+++ src/main/java/com/example/demo/service/AsyncService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName: AsyncService
+ * @Author: feng.i.liu
+ * @Description:
+ * @Date: 2021/7/1 17:19
+ * @Version: 1.0
+ */
+@Service
+public class AsyncService {
+
+    @Async
+    public void hello(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("业务进行中....");
+    }
+}
